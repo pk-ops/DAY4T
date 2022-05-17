42f2e4dad58e2b5762706657dd0bc11e38d9b8dc
pk-ops <71349399+pk-ops@users.noreply.github.com>
2022-05-17 12:13:22 +0530

    Add files via upload
    UPDATED

--- DAY4Q2.js
+++ DAY4Q2.js
@@ -4,7 +4,7 @@ request.send();
 request.onload=function(){
     var dataa=JSON.parse(request.response);
     console.log(dataa);
-    //for(var i=0;i<dataa.length;i++){
-     //   console.log(dataa[i].flag);
-   // }
+    for(var i=0;i<dataa.length;i++){
+        console.log(dataa[i].flag);
+    }
 }
